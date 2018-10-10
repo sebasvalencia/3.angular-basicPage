1b53d9beb4cf9954a3e0c71aa6dc8318aa84ec3c
Sebastián Valencia <sebastian.valencian@gmail.com>
2018-10-09 20:11:25 -0500

    [Refactor]Modify header, body, index, styles,

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BodyComponent implements OnInit {
 
   firstDog = 'https://images.dog.ceo/breeds/hound-basset/n02088238_12160.jpg';
-  secondDog = 'https://images.dog.ceo/breeds/beagle/n02088364_11130.jpg';
+
   constructor() { }
 
   ngOnInit() {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  companyName = 'Angular Company';
+  companyName = `Dog's Company`;
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
